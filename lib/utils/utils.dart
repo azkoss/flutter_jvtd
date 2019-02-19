153c467076387a0c677b82d5590ff7fb37adf836
陈磊的MacBook Pro <200892907@qq.com>
2019-02-19 16:04:36 +0800

    添加省份工具类

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,3 +2,4 @@ export 'date_util.dart';
 export 'match_util.dart';
 export 'timer_util.dart';
 export 'local_storage_util.dart';
+export 'provinces_util.dart';
--- test/flutter_jvtd_test.dart
+++ test/flutter_jvtd_test.dart
@@ -1,6 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_jvtd/flutter_jvtd.dart';
 
-void main() {
-
-}
+void main() {}
