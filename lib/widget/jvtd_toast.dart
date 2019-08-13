baf25319cda5cb1053a6568e949c4de146f7a0f2
陈磊的MacBook Pro <200892907@qq.com>
2019-08-13 11:12:20 +0800

    修改默认属性toast

--- lib/widget/jvtd_toast.dart
+++ lib/widget/jvtd_toast.dart
@@ -10,20 +10,32 @@ class JvtdToast {
   /// toast 显示时长 默认 LENGTH_SHORT [LENGTH_SHORT（ios 1秒）/LENGTH_LONG（ios 2秒）]
   /// bgColor 背景颜色  默认黑色
   /// textColor 字体颜色  默认白色
-  static void showMessage({
-    @required String msg,
-    @required BuildContext context,
-    int toastGravity = 0,
-    int toast = 0,
-    Color bgColor = Colors.black,
-    Color textColor = Colors.white,
-  }) {
+  /// radius 圆角 8
+  /// border 边缘
+  static void showMessage(
+      {@required String msg,
+      @required BuildContext context,
+      int toastGravity = 0,
+      int toast = 0,
+      Color bgColor = const Color(0xAA000000),
+      Color textColor = Colors.white,
+      double radius = 8,
+      Border border}) {
     if (toastGravity == 0) {
       toastGravity = Toast.BOTTOM;
     }
     if (toast == 0) {
       toast = Toast.LENGTH_SHORT;
     }
-    Toast.show(msg, context);
+    Toast.show(
+      msg,
+      context,
+      duration: toast,
+      gravity: toastGravity,
+      backgroundColor: bgColor,
+      textColor: textColor,
+      backgroundRadius: radius,
+      border: border,
+    );
   }
 }
